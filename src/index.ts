33c562ab1c438d52c574340bf760c84b48862270
Gopala Agrawal <agrawalgopala@gmail.com>
2024-10-07 10:49:03 +0530

    feat: helow world route

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+// for accessing the environment variables
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+//TODO: have to figure out more cors configuration
+app.use(cors());
+//TODO: have to check how exactly the json payloads are parsed
+// How they look before and after parsing
+app.use(express.json()); // parsing this incoming requests with json payloads
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
